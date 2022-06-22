0d88f2aef2f7c5b0cb96998648257879acbb14a2
Maja Gedda <maja.gedda@zocom.se>
2022-06-22 11:57:51 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+      <h1>Poster Shop</h1>
+      <img alt="ninja head header" src="../assets/images/ninjahead.svg">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
